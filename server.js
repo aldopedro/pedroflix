a7422a6542e72ad3c62b3d4df522fd56e47571ef
aldopedro <pedroaldo2025@gmail.com>
2024-11-30 00:02:39 -0300

    inicio do desenvolvimento do token (cookies e auth)

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ var mysql = require('mysql')
 const dotenv = require('dotenv')
 dotenv.config()
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 
+
+const session = {}
 const con = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -13,17 +16,27 @@ const con = mysql.createConnection({
 con.connect()
 var app = express();
 app.use(express.json())
+app.use(cookieParser())
 const corsOptions = {
     origin: "*",
     optionsSuccessStatus: 200,
 };
+const teste = function (req, res, next) {
 
+    console.log("testou")
+    next()
+}
 app.use(cors(corsOptions))
 
 app.get('/', (req, res) => {
     con.query("SELECT * FROM users", (err, result) => {
         res.send(result);
     })
+})
+
+app.get('/set-cookie', (req, res) => {
+
+    return res.json()
 })
 
 app.post('/add_user', async (req, res) => {
@@ -41,6 +54,9 @@ app.post('/add_user', async (req, res) => {
         })
     }
 })
+
+
+
 app.post('/login', async (req, res) => {
     const emailRegex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
     const email = (req.body.email)
@@ -51,7 +67,13 @@ app.post('/login', async (req, res) => {
             con.query(`SELECT * FROM users WHERE password = ?`, [password], (err1, result1) => {
                 if (result[0] === undefined || result1[0] === undefined) {
                     console.log("nao deu certo")
-                } else console.log("deu certo.")
+                    return res.json("false")
+                } else 
+                console.log("deu certo.")
+                res.cookie("teste","teste", {
+                    maxAge: 1* 60 * 60
+                })
+                return res.json("true")
             })
         })
     }
