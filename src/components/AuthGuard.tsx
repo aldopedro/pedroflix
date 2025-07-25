fcfeb663cefe5c45cc5052b2b20bdd818a34f68f
aldopedro <pedroaldo2025@gmail.com>
2025-07-25 13:45:23 -0300

    ajust render

--- src/components/AuthGuard.tsx
+++ src/components/AuthGuard.tsx
@@ -8,17 +8,13 @@ type Props = {
 };
 
 export default function AuthGuard({ children }: Props) {
-  const API_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://pedroflix-api.onrender.com"
-    : "http://localhost:8080";
   const router = useRouter();
   const [authorized, setAuthorized] = useState<boolean | null>(null);
 
   useEffect(() => {
     const validateToken = async () => {
       try {
-        const response = await fetch(`${API_URL}/validate`, {
+        const response = await fetch(`https://pedroflix-api.onrender.com/validate`, {
           method: 'GET',
           credentials: 'include',
         });
